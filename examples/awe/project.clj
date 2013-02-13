554b90c63a21d5042cdc801e2a8b7350a528c599
tgk <tgk@trifork.com>
2013-02-13 11:04:18 +0100

    Use clojure.edn/read and clojure.edn/read-string, available from 1.5.0.

--- examples/awe/project.clj
+++ examples/awe/project.clj
@@ -1,7 +1,7 @@
 (defproject awesomeness "0.0.1"
   :description "true power awesomeness"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0-RC15"]
                  [ring "1.0.2"]
                  [compojure "1.0.1"]
-                 [fogus/ring-edn "0.1.0"]]
+                 [fogus/ring-edn "0.1.0-SNAPSHOT"]]
   :main awesome-app)
