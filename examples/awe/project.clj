271cc2e5fb1543c63d44f737da503826a2eaf73f
fogus <mefogus@gmail.com>
2013-02-13 08:56:47 -0500

    Bumping version

--- examples/awe/project.clj
+++ examples/awe/project.clj
@@ -3,5 +3,5 @@
   :dependencies [[org.clojure/clojure "1.5.0-RC15"]
                  [ring "1.0.2"]
                  [compojure "1.0.1"]
-                 [fogus/ring-edn "0.1.0-SNAPSHOT"]]
+                 [fogus/ring-edn "0.2.0-SNAPSHOT"]]
   :main awesome-app)
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fogus/ring-edn "0.1.0-SNAPSHOT"
+(defproject fogus/ring-edn "0.2.0-SNAPSHOT"
   :description "A Ring middleware that augments :params by parsing a request body as Extensible Data Notation (EDN)."
   :dependencies [[org.clojure/clojure "1.5.0-RC15"]]
   :plugins [[lein-swank "1.4.4"]
