cd31b60f00adfdb1fffa7b58c84983db789d64d8
Fogus <mefogus@gmail.com>
2013-03-05 08:14:39 -0500

    Update project.clj

--- examples/awe/project.clj
+++ examples/awe/project.clj
@@ -1,6 +1,6 @@
 (defproject awesomeness "0.0.1"
   :description "true power awesomeness"
-  :dependencies [[org.clojure/clojure "1.5.0-RC15"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [ring "1.0.2"]
                  [compojure "1.0.1"]
                  [fogus/ring-edn "0.2.0-SNAPSHOT"]]
