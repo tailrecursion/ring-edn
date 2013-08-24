bf64bfb2fb35d680cfa57e95662005cc77cc50b9
fogus <mefogus@gmail.com>
2013-08-24 10:40:23 -0400

    Removed Marg dep.  Closes #3

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
 (defproject fogus/ring-edn "0.2.0"
   :description "A Ring middleware that augments :params by parsing a request body as Extensible Data Notation (EDN)."
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :plugins [[lein-swank "1.4.4"]
-            [lein-marginalia "0.8.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]])
