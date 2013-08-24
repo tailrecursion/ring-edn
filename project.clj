e36e454106f4ae95fe732420e80bc6ea4ec70856
fogus <mefogus@gmail.com>
2013-08-24 10:35:43 -0400

    Bumped to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fogus/ring-edn "0.2.0-SNAPSHOT"
+(defproject fogus/ring-edn "0.2.0"
   :description "A Ring middleware that augments :params by parsing a request body as Extensible Data Notation (EDN)."
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[lein-swank "1.4.4"]
