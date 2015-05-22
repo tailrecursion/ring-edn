d5cd3f4b72e11850625d77ab3029f55623014b17
fogus <mefogus@gmail.com>
2015-05-22 09:53:11 -0400

    Added :license and :url

--- project.clj
+++ project.clj
@@ -1,3 +1,8 @@
 (defproject fogus/ring-edn "0.4.0-SNAPSHOT"
   :description "A Ring middleware that augments :params by parsing a request body as Extensible Data Notation (EDN)."
+  :url "https://github.com/tailrecursion/ring-edn"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
